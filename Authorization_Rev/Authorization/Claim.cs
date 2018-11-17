fbb4b2ba32eee706f9a9b12c7cf1ac478e8984dd
nguyentrong56 <33231414+nguyentrong56@users.noreply.github.com>
2018-11-16 23:03:21 -0800

    Revision of Authorization
    I remove DataAccessLayer and database to simplify the implementation and  add a checkClaim function in class Controller.
    
    At initial , I assign claims to usesr in main program which can later be done by an user managament . Then I have the users try to access the feature which they are and are not allowed.

---
+++ Authorization_Rev/Authorization/Claim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization
+{
+    public class Claim
+    {
+        public Claim(String _claimType, bool _claimValue)
+        {
+            ClaimType = _claimType;
+            ClaimValue = _claimValue;
+        }
+
+        public String ClaimType { get; set; }
+        public bool ClaimValue { get; set; }
+    }
+}
---
+++ Authorization_Rev/Authorization/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Create user Trong 
+            User Trong = new User("Trong", "Student");
+            //Assign claim CanDeleteUserOwnAccount
+            Trong.addClaim("CanDeleteUserOwnAccount", true);
+
+            //Create controller and use it to authorize user
+            Controller c1 = new Controller(Trong);
+            Console.WriteLine("***Let Trong delete his own user account***");
+
+            //Access DeleteUserOwnAccount method using the controller
+            c1.DeleteUserOwnAccount();
+
+            Console.WriteLine("***Let Trong delete other user account***");
+
+            //Access DeleteOtherAccount method using the controller
+            c1.DeleteOtherAccount("Krystal");
+
+            //Create user Krystal 
+            User Krystal = new User("Krystal", "Admin");
+
+            //Assign CanDeleteUserOwnAccount and CanDeleteUserOwnAccount
+            Krystal.addClaim("CanDeleteUserOwnAccount", true);
+            Krystal.addClaim("CanDeleteUserOwnAccount", true);
+
+            Controller c2 = new Controller(Krystal);
+
+            Console.WriteLine("\n***Let Krystal delete her own user account***");
+            c2.DeleteUserOwnAccount();
+
+            Console.WriteLine("***Let Krystal delete other user account***");
+            c2.DeleteOtherAccount("Krystal");
+
+            Console.ReadLine();
+
+
+           
+        }
+    }
+}
