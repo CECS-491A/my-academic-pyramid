af3d2055978615a14d56ff7f18a19f10dcd4af5c
Victor Kim <victorkim95@gmail.com>
2018-11-21 14:51:11 -0800

    Victor Kim Sha1 GetHash revision
    Re-edited some of the files and trying to upload again since the last time it didn't work out.

--- PasswordChecking/HashDemo.test/HashTest.cs
+++ PasswordChecking/HashDemo.test/HashTest.cs
@@ -10,10 +10,11 @@ namespace HashDemo.test
 {
     public class HashTest
     {
+        SHA1HashFunction Sha1 = new SHA1HashFunction();
         [Fact]
-        public void GetHashValue_CheckByteValue()
+        public void GetHashValue_CheckIfValid()
         {
-            SHA1HashFunction Sha1 = new SHA1HashFunction();
+            
             //Arrange
             string expected = "25E6A14076898A344AB680E2A589A09885EB04C2";
             //Act
@@ -22,5 +23,20 @@ namespace HashDemo.test
             //Assert
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void GetHashValue_CheckIfInvalid()
+        {
+            //Arrange
+            string expected = "25E6A14076898A344AB680E2A589A09885EB04C2";
+            string input = "Polar";
+            //Act
+
+            string actual = Sha1.GetHashValue(input);
+            //Assert
+            Assert.NotEqual(expected, actual);
+        }
+
+
     }
 }
---
+++ PasswordChecking/PasswordChecking.Tests/Program.cs
@@ -0,0 +1,30 @@
+﻿using PasswordChecking.HashFunctions;
+using System;
+using System.Net;
+
+namespace PasswordChecking
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Use SHA1 Hash Function
+            SHA1HashFunction sha = new SHA1HashFunction();
+
+            // Make Password Validation Object
+            string password = "password";
+            Console.WriteLine("Password: " + password);
+            string url = "https://api.pwnedpasswords.com/range/";
+            Console.WriteLine("URL: " + url);
+            WebClient client = new WebClient();
+            PasswordValidation pv = new PasswordValidation(sha, password, client, url);
+
+            // Run validation and return hash object
+            int hashCount = pv.Run();
+            Console.WriteLine("Count: " + hashCount);
+            Console.WriteLine("End");
+            Console.ReadKey(true);
+        }
+    }
+}
