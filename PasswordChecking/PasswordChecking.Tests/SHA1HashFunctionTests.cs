e67bc67d955407cd1a7c69c6071c1bf2a4d99f62
Victor Kim <victorkim95@gmail.com>
2018-11-25 14:43:07 -0800

    Updated Sha1HashFunction Comments
    Changed few comments for each method.

--- PasswordChecking/PasswordChecking.Tests/SHA1HashFunctionTests.cs
+++ PasswordChecking/PasswordChecking.Tests/SHA1HashFunctionTests.cs
@@ -8,12 +8,15 @@ using Xunit;
 
 namespace HashDemo.test
 {
+
     public class HashTest
     {
         //Use the Sha1 Hash Function
         SHA1HashFunction Sha1 = new SHA1HashFunction();
 
-        //Method checks if input matches the expected Hash Value
+        /// <summary>
+        /// Methods checks if input matches the expected Hash Value
+        /// </summary>        
         [Fact]
         public void GetHashValue_CheckIfValid()
         {
@@ -21,21 +24,27 @@ namespace HashDemo.test
             //Arrange
             string expected = "25E6A14076898A344AB680E2A589A09885EB04C2"; //Expected Hash Value
             string input = "PolarIceCaps";                                //Example input
+            
             //Act
             string actual = Sha1.GetHashValue(input);                     //Gets Hash Value from input using Sha1.GetHashValue
+            
             //Assert
             Assert.Equal(expected, actual);                               //Checks if actual and expected are the same
         }
-        //Method checks if input does not match the expected Hash Value
+
+        /// <summary>
+        /// //Method checks if input does not match the expected Hash Value
+        /// </summary>
         [Fact]
         public void GetHashValue_CheckIfInvalid()
         {
             //Arrange
             string expected = "25E6A14076898A344AB680E2A589A09885EB04C2"; //Expected Hash Value
             string input = "Polar1ceCap";                                 //Example input
+           
             //Act
-
             string actual = Sha1.GetHashValue(input);                     //Gets Hash Value from input using Sha1.GetHashValue
+            
             //Assert
             Assert.NotEqual(expected, actual);                            //Checks if actual and expected are not the same
         }
