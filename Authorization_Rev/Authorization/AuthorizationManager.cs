b35436f7b3072e5991a915a450903b4876f9001e
Luis Julian <ljulian2190@gmail.com>
2018-11-25 15:11:27 -0800

    Seperated the Authorization class from UserManagment to make the Authorization class standalone. Also added a mock Controller class to demonstrate how the new AuthorizationManager would be used.

---
+++ Authorization_Rev/Authorization/AuthorizationManager.cs
@@ -0,0 +1,43 @@
+﻿using Authorization.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Authorization
+{
+    public class AuthorizationManager:IAuthorizationManager
+    {
+        
+        private User authorizedUser;
+
+        public AuthorizationManager(User user)
+        { 
+            authorizedUser = user;
+        }
+
+
+
+        public bool CheckClaims(List<String> requiredClaimTypes)
+        {
+            
+            // To check if a claim type exists in user's claim list and the claim value is true
+            if (requiredClaimTypes.All(rc =>
+            {
+                // body of lambda function
+                Claim foundClaim = authorizedUser.userClaims.Find(uc => uc.ClaimType.Equals(rc));
+                if (foundClaim != null && foundClaim.ClaimValue == true)
+                    return true;
+                else
+                    return false;
+
+            }))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
