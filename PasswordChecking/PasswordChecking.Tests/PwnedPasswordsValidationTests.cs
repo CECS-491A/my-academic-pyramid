408717abf3b9694d7ed30e708800cfe07abba056
Hyunwoo Kim <KevinError@users.noreply.github.com>
2018-11-27 19:55:53 -0800

    MoreReadableUnitTesting

--- PasswordChecking/PasswordChecking.Tests/PwnedPasswordsValidationTests.cs
+++ PasswordChecking/PasswordChecking.Tests/PwnedPasswordsValidationTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace PasswordChecking.Tests
 {
-    public class PasswordValidationTests
+    public class PwnedPasswordsValidationTests
     {
         [Fact]
         public void FindHash_FoundShouldReturnCount()
@@ -163,59 +163,6 @@ namespace PasswordChecking.Tests
             Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void RequestData_ShouldPass()
-        {
-            // Arrange
-            SHA1HashFunction sha = new SHA1HashFunction();
-            string password = "password";
-            string url = "https://api.pwnedpasswords.com/range/";
-            PwnedPasswordsValidation HttpClientMethods = new PwnedPasswordsValidation(sha, password, url);
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-            bool expected = true;
-            bool actual;
-
-            //Act
-            try
-            {
-                //Task<string> response = HttpClientMethods.RequestData("https://api.pwnedpasswords.com/range/5BAA6");
-                actual = true;
-            }
-            catch (WebException e)
-            {
-                actual = false;
-            }
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void RequestData_InvalidUrlShouldFail()
-        {
-            // Arrange
-            SHA1HashFunction sha = new SHA1HashFunction();
-            string password = "password";
-            string url = "https://api.pwnedpasswords.com/range/";
-            PwnedPasswordsValidation pv = new PwnedPasswordsValidation(sha, password, url);
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-            bool expected = true;
-            bool actual;
-
-            //Act
-            try
-            {
-                //Task<string> response = HttpClientMethods.RequestData("should throw exception");
-            }
-            catch (WebException e)
-            {
-                actual = true;
-                //Assert
-                Assert.Equal(expected, actual);
-            }
-        }
     }
 }
 
