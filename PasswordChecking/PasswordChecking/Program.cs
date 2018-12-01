760dedb1fc110c5d4d4dd3aa47f7808f371cf551
Hyunwoo Kim <KevinError@users.noreply.github.com>
2018-12-01 05:34:00 -0800

    DecreaseRunTime

--- PasswordChecking/PasswordChecking/Program.cs
+++ PasswordChecking/PasswordChecking/Program.cs
@@ -8,7 +8,7 @@ namespace PasswordChecking
     {
         private static SHA1HashFunction sha = new SHA1HashFunction();
         private static string url = "https://api.pwnedpasswords.com/range/";
-        private static PwnedPasswordsCount pv = new PwnedPasswordsCount(sha, url);
+        private static PwnedPasswordsValidation pv = new PwnedPasswordsValidation(sha, url);
 
         static void Main(string[] args)
         {
@@ -19,24 +19,28 @@ namespace PasswordChecking
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            // Get password count
-            int hashCount = pv.GetCount(password);
-            Console.WriteLine("Count: " + hashCount);
+            for (int i = 0; i < 1000; i++)
+            {
+                // Get password count
+                int hashCount = pv.GetCount(password);
+                //Console.WriteLine("Count: " + hashCount);
+                if(hashCount < 0)
+                {
+                    Console.WriteLine("Invalid Values");
+                }
+                else
+                {
+                    // Check Business Rules
+                    //PasswordCheckingBR.CheckPasswordCount(hashCount);
+                }
 
-            if(hashCount < 0)
-            {
-                Console.WriteLine("Invalid Values");
-            }
-            else
-            {
-                // Check Business Rules
-                PasswordCheckingBR.CheckPasswordCount(hashCount);
             }
 
             sw.Stop();
             Console.WriteLine("Duration: " + sw.ElapsedMilliseconds + " ms");
             Console.WriteLine("End");
-            Console.ReadKey(true);
+            Console.ReadKey(true);//80005 ms, 80643, 79484
+            // 75954, 87209, 76206
         }
     }
 }
