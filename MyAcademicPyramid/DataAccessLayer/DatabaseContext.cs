248067c6a114fe813daa22e2de9b8134f22e0eb7
Trong Nguyen <33231414+nguyentrong56@users.noreply.github.com>
2018-12-06 13:27:52 -0800

    Update DatabaseContext.cs

--- MyAcademicPyramid/DataAccessLayer/DatabaseContext.cs
+++ MyAcademicPyramid/DataAccessLayer/DatabaseContext.cs
@@ -4,7 +4,10 @@ namespace DataAccessLayer
 {
     public class DatabaseContext : DbContext
     {
+        // Create a DbSet of user objects
         public virtual DbSet<User> Users { get; set; }
+        
+        //Create a Dbset of claim objects 
         public virtual DbSet<Claim> Claims { get; set; }
 
 
