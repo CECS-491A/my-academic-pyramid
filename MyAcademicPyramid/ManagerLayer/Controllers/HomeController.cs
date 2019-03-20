9b3d93b060542353ad37bd21c0fb7441dfde8345
Luis Julian <ljulian2190@gmail.com>
2019-03-19 17:27:38 -0700

    Beginning creating action for login.

--- MyAcademicPyramid/ManagerLayer/Controllers/HomeController.cs
+++ MyAcademicPyramid/ManagerLayer/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using SecurityLayer;
+using DataAccessLayer;
+using DataAccessLayer.DTOs;
+using ManagerLayer.UserManagement;
+using ServiceLayer.PasswordChecking.HashFunctions;
 
 namespace ManagerLayer.Controllers
 {
@@ -18,18 +22,63 @@ namespace ManagerLayer.Controllers
             return "Hello World From Backend API " + DateTime.Now;
         }
 
+        private void CreateUsers()
+        {
+            User user1 = new User()
+            {
+                UserName = "Abc@gmail.com",
+                Firstname = "Jackie",
+                LastName = "Chan"
+            };
+
+            User user2 = new User()
+            {
+                UserName = "tri@yahoo.com",
+                Firstname = "David",
+                LastName = "Gonzales"
+            };
+
+            User user3 = new User()
+            {
+                UserName = "Smith@gmail.com",
+                Firstname = "Michael",
+                LastName = "Nguyen"
+            };
+
+            DatabaseContext db = new DatabaseContext();
+            UserManager uM = new UserManager(db);
+            uM.CreateUserAction(user1, "eoifj");
+            uM.CreateUserAction(user2, "eoifj");
+            uM.CreateUserAction(user3, "eoifj");
+            db.SaveChanges();
+        }
+
         [HttpGet]
         [Route("Home/TestJWT")]
-        public string TestJWT()
+        public string TestJWT([FromBody] string username)
         {
-            Dictionary<string, string> testDict = new Dictionary<string, string>()
-            {
-                {"User", "ljulian2190@gmail.com" },
-                {"Claims", "[Student, Tutor]" }
-            };
-            return "test";
 
-            //return JWTokenManager.GenerateToken(testDict);
-        }
+            /* Create three users. 
+             * Create 
+             */
+
+            CreateUsers();
+            DatabaseContext db = new DatabaseContext();
+            JWTokenManager tm = new JWTokenManager(db);
+
+            //string token = tm.GenerateToken();
+            //return Ok(user);
+            return "";
+    //Dictionary<string, string> testDict = new Dictionary<string, string>()
+    //{
+    //    {"User", "ljulian2190@gmail.com" },
+    //    {"Claims", "[Student, Tutor]" }
+    //};
+
+
+    //return JWTokenManager.GenerateToken(testDict);
+    }
+
+
     }
 }
