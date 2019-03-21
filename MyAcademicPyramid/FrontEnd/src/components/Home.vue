6c004c6e7c94217f4312db0f7204ae23d13a869a
arturopenacontreras <32249177+arturopenacontreras@users.noreply.github.com>
2019-03-20 17:20:45 -0700

    Update Home.vue

--- MyAcademicPyramid/FrontEnd/src/components/Home.vue
+++ MyAcademicPyramid/FrontEnd/src/components/Home.vue
@@ -1,44 +1,17 @@
 <template>
-  <div class="hello">
+  <div class="home">
     <h1>{{ info }}</h1>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+//import axios from 'axios'
 export default {
   data () {
     return {
-      info: null,
+      info: 'My Academic Pyramid',
     }
-  },
-  mounted () {
-    axios
-      .get('http://localhost:60500/api/logging/geterrors', {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => (this.info = response.data))
-      
   }
+  
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
