e95d97c1888da06b072ee5bf1e047b5dae8a30c8
Luis Julian <ljulian2190@gmail.com>
2019-03-23 20:29:29 -0700

    Added class to represent payload from SSO

---
+++ BackEnd/KFC.SIT.WebAPI/Controllers/LogoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SecurityLayer;
+using DataAccessLayer;
+using DataAccessLayer.DTOs;
+using ManagerLayer.UserManagement;
+
+namespace KFC.SIT.WebAPI.Controllers
+{
+    public class LogoutController : ApiController
+    {
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+            // TODO invalidate the token.
+            DatabaseContext db = new DatabaseContext();
+            JWTokenManager jm = new JWTokenManager(db);
+            jm.InvalidateToken(value);
+        }
+    }
+}
