df8cd2ece53df13e459472e6a15dfc75bbb46f85
Kevin <khk4084417@gmail.com>
2019-04-15 08:43:24 -0700

    Dashboard

--- BackEnd/KFC.SIT.ServiceLayer/DataAnalysisDashboard/DashboardService.cs
+++ BackEnd/KFC.SIT.ServiceLayer/DataAnalysisDashboard/DashboardService.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DataAccessLayer;
+using DataAccessLayer.Logging;
+using DataAccessLayer.Models;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace ServiceLayer.DataAnalysisDashboard
 {
     public class DashboardService
     {
+        private readonly IMongoCollection<TelemetryLog> Collection;
+
+        private const string _collectionName = "TelemetryLogs";
+
+        MongoDBRepo _repo = new MongoDBRepo("mongodb+srv://super:superheroes@myacademicpyramidlogging-if0cx.mongodb.net/test?retryWrites=true", "test");
+
+        public DashboardService()
+        {
+            this.Collection = _repo.Db.GetCollection<TelemetryLog>(_collectionName);
+        }
     }
 }
