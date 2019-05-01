f5c605cfe3df7c5d9411e4c934c94715b87b37f7
Kevin <khk4084417@gmail.com>
2019-05-01 07:22:44 -0700

    UI ^^

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -18,6 +18,14 @@
     </v-toolbar>
   
     <v-navigation-drawer app v-model="drawer" class="info">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="./assets/Vong.jpg">
+          </v-avatar>
+          <p class="white--text subheading mt-1">Professor Vong</p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
