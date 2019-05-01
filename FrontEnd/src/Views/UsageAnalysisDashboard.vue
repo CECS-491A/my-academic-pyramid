a7e34da3c12c50125a171dd8fdb5497fe657a3ce
Kevin <khk4084417@gmail.com>
2019-05-01 06:02:57 -0700

    UI

--- FrontEnd/src/Views/UsageAnalysisDashboard.vue
+++ FrontEnd/src/Views/UsageAnalysisDashboard.vue
@@ -1,20 +1,44 @@
 <template>
-  <div class="small">
-    Hello
-  </div>
+  <section class="container">
+    <h1>Demo examples of vue-charts</h1>
+    <div class="columns">
+      <div class="column">
+        <h3>Line Chart</h3>
+        <chartjs-line></chartjs-line>
+      </div>
+      <div class="column">
+        <h3>Bar Chart</h3>
+        <chartjs-bar></chartjs-bar>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <h3>Radar Chart</h3>
+        <!--Radar Chart Example-->
+      </div>
+      <div class="column">
+        <h3>Data Binding Line Chart</h3>
+        <!--Data Binding Line Chart Example-->
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
-  //import LineChart from "./componenets/Dashboard/LineChart.vue";
 
+<script>
   export default {
-    
+    name: 'VueCharts',
+    components: {
+    },
+    data () {
+      return {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        dataset: [65, 59, 80, 81, 56, 55, 40]
+      }
+    }
   }
 </script>
 
 <style>
-  .small {
-    max-width: 600px;
-    margin:  150px auto;
-  }
+
 </style>
