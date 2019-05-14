98f81a005c6c8a98ee8f4edbd08ee0e489d0b2f4
Victor <victorckim1995@gmail.com>
2019-05-13 19:33:00 -0700

    Separated Archiving into layers: Manager and Service.

---
+++ BackEnd/KFC.SIT.ManagerLayer/Archiving/ArchivingManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessLayer.Models;
+using System.IO;
+using System.IO.Compression;
+using ServiceLayer.Archiving;
+
+namespace ManagerLayer.Archiving
+{
+    public class ArchivingManager
+    {
+        private static string archivePath = @"My-Academic-Pyramid\Archives";
+        ArchiveServices ArchLogs = new ArchiveServices();
+        private int errorcount = 0;
+        private const int maxLogLife = 30;
+        public bool ArchiveOldLogFiles()
+        {
+
+            bool successfulArchive = false;
+            bool successfulDelete = false;
+            List<string> ErrorLogs = ArchLogs.GetOldErrorLogs(maxLogLife);
+            List<string> TelemetryLogs = ArchLogs.GetOldTelemetryLogs(maxLogLife);
+            string telemetryArchiveName = DateTime.Now.ToString("dd-MM-yyyy") + "_TelemetryArchive.zip";
+            string errorArchiveName = DateTime.Now.ToString("dd-MM-yyyy") + "_ErrorArchive.zip";
+
+
+            try
+            {
+                using (FileStream filestreamer = new FileStream((archivePath + telemetryArchiveName), FileMode.OpenOrCreate))
+                {
+                    using (ZipArchive archiver = new ZipArchive(filestreamer, ZipArchiveMode.Update))
+                    {
+                        foreach (string logId in TelemetryLogs)
+                        {
+                            TelemetryLog telemetryFile = ArchLogs.GetTelemetryLog(logId);
+                            ZipArchiveEntry logEntry = archiver.CreateEntry(telemetryFile.ToString());
+
+                            successfulDelete = ArchLogs.RemoveTelemetryLog(logId);
+                        }
+                        if (successfulDelete == true)
+                        {
+                            archiver.Dispose();
+                            successfulArchive = true;
+                        }
+
+                    }
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                successfulArchive = false;
+                errorcount++;
+            }
+            try
+            {
+                using (FileStream filestreamer = new FileStream((archivePath + errorArchiveName), FileMode.OpenOrCreate))
+                {
+                    using (ZipArchive archiver = new ZipArchive(filestreamer, ZipArchiveMode.Update))
+                    {
+                        foreach (string logId in ErrorLogs)
+                        {
+                            ErrorLog errorFile = ArchLogs.GetErrorLog(logId);
+                            ZipArchiveEntry logEntry = archiver.CreateEntry(errorFile.ToString());
+
+                            successfulDelete = ArchLogs.RemoveErrorLog(logId);
+                        }
+                        if (successfulDelete == true)
+                        {
+                            archiver.Dispose();
+                            successfulArchive = true;
+                        }
+
+                    }
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                successfulArchive = false;
+                errorcount++;
+            }
+
+            return successfulArchive;
+        }
+
+
+        public void contactAdmin()
+        {
+            if (errorcount >= 3)
+            {
+                //System admin notified.
+                errorcount = 0;
+            }
+        }
+    }
+}
---
+++ BackEnd/KFC.SIT.ServiceLayer/Archiving/ArchiveServices.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessLayer.Models;
+using DataAccessLayer.Logging;
+using System.IO;
+
+namespace ServiceLayer.Archiving
+{
+    public class ArchiveServices
+    {
+        private static ErrorLogCollection _errors = new DataAccessLayer.Logging.ErrorLogCollection();
+        private static TelemetryLogCollection _telemetries = new DataAccessLayer.Logging.TelemetryLogCollection();
+        private static int errorcount = 0;
+
+        public TelemetryLog GetTelemetryLog(string Id)
+        {
+            return _telemetries.Get(Id);
+        }
+
+        public ErrorLog GetErrorLog(string Id)
+        {
+            return _errors.Get(Id);
+        }
+
+        // GET: Archiving
+        public List<string> GetErrorLogs()
+        {
+
+            List<ErrorLog> allErrorLogs = _errors.GetAll();
+            List<string> errorLogList = new List<string>();
+            foreach (ErrorLog errorpath in allErrorLogs)
+            {
+                errorLogList.Add(errorpath.ID);
+            }
+            return errorLogList;
+        }
+
+        public List<string> GetTelemetryLogs()
+        {
+            List<TelemetryLog> allTelemetryLogs = _telemetries.GetAll();
+            List<string> telemetryLogList = new List<string>();
+            foreach (TelemetryLog telemetryPath in allTelemetryLogs)
+            {
+                telemetryLogList.Add(telemetryPath.ID);
+            }
+            return telemetryLogList;
+        }
+
+
+        public List<String> GetOldErrorLogs(int maxLogLife)
+        {
+            List<string> logList = GetErrorLogs();
+            List<string> oldErrorLogList = new List<string>();
+            foreach (string logID in logList)
+            {
+                if (checkOldErrorLog(logID, maxLogLife) == true)
+                {
+                    oldErrorLogList.Add(logID);
+                }
+            }
+            return oldErrorLogList;
+        }
+        public List<String> GetOldTelemetryLogs(int maxLogLife)
+        {
+            List<string> logList = GetTelemetryLogs();
+            List<string> oldTelemetryLogList = new List<string>();
+            foreach (string logID in logList)
+            {
+                if (checkOldTelemetryLog(logID, maxLogLife) == true)
+                {
+                    oldTelemetryLogList.Add(logID);
+                }
+            }
+            return oldTelemetryLogList;
+        }
+        public bool checkOldErrorLog(string fileId, int maxLogLife)
+        {
+            ErrorLog errorfile = _errors.Get(fileId);
+            bool OldLog = false;
+            if ((DateTime.Now - errorfile.Date).TotalDays > maxLogLife)
+            {
+                OldLog = true;
+            }
+            return OldLog;
+
+        }
+        public bool checkOldTelemetryLog(string fileId, int maxLogLife)
+        {
+            TelemetryLog errorfile = _telemetries.Get(fileId);
+            bool OldLog = false;
+            if ((DateTime.Now - errorfile.Date).TotalDays > maxLogLife)
+            {
+                OldLog = true;
+            }
+            return OldLog;
+
+        }
+
+        public bool RemoveTelemetryLog(string logId)
+        {
+            bool isSuccessfulRemoval = false;
+            try
+            {
+                _telemetries.Delete(logId);
+                isSuccessfulRemoval = true;
+            }
+            catch (FileNotFoundException e)
+            {
+                isSuccessfulRemoval = false;
+                errorcount++;
+            }
+
+            return isSuccessfulRemoval;
+        }
+        public bool RemoveErrorLog(string logId)
+        {
+            bool isSuccessfulRemoval = false;
+            try
+            {
+                _telemetries.Delete(logId);
+                isSuccessfulRemoval = true;
+            }
+            catch (FileNotFoundException e)
+            {
+                isSuccessfulRemoval = false;
+                errorcount++;
+            }
+
+            return isSuccessfulRemoval;
+        }
+    }
+}
