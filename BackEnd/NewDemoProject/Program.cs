6cc1b7109d42fbcb245c3bebda4f30e0f665528d
Kevin <khk4084417@gmail.com>
2019-05-15 12:35:42 -0700

    minor change

--- BackEnd/NewDemoProject/Program.cs
+++ BackEnd/NewDemoProject/Program.cs
@@ -19,27 +19,12 @@ namespace NewDemoProject
         {
             DashboardManager manager = new DashboardManager(url, database);
             DashboardService service = new DashboardService(url, database);
-            IDictionary<int, long> get = service.CountSuccessfulLogin(6);
+            IDictionary<int, long> get = service.CountSuccessfulLogin(24);
             IDictionary<int, long> get2 = service.CountFailedLogin(12);
             //IList<Double> get3 = manager.GetAverageSuccessfulLogin();
             IDictionary<int, long> get4 = service.CountSuccessfulLogin(1);
-            foreach (var s in get)
-            {
-                Console.WriteLine(s);
-            }
-            Console.WriteLine("Failed");
-            foreach (var s in get2)
-            {
-                Console.WriteLine(s);
-            }
-            IDictionary<int, long> temp6 = new Dictionary<int, long>();
-            temp6.Add(0, 2);
-            temp6.Add(1, 3);
-            temp6.Add(2, 4);
-            temp6.Add(3, 5);
-            temp6.Add(4, 6);
 
-            IDictionary<int, long> test3 = service.CountSuccessfulLogin(6);
+            IDictionary<int, long> test3 = service.CountFailedLogin(6);
             foreach(var s in test3)
             {
                 Console.WriteLine("Is heere: " + s);
